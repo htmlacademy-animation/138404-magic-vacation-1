824fc267126d03185a84cbc73666d8883915e54a
soloduhin <Nikolay.Solodukhin@gmail.com>
2020-05-15 23:06:16 +0300

    Сделал задание module2-task3

---
+++ source/js/modules/history.js
@@ -0,0 +1,16 @@
+import AccentTypography from './accent-typography';
+
+const ScreenStory = document.querySelector(`.screen--story`);
+const HistoryTitle = ScreenStory.querySelector(`.slider__item-title`);
+
+const HistoryTitleAccentTypography = new AccentTypography(
+  HistoryTitle,
+  650,
+  {min: 0, max: 150},
+  `active`,
+  `transform`,
+);
+
+export {
+  HistoryTitleAccentTypography
+};
